9139c24f0b4614f910f3ad95e03e79a5970598c8
Jacob O'Keeffe <jacob.okeeffe22@mail.dcu.ie>
2015-08-18 01:17:03 +0100

    added ajax functionality to site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$( ".nav li" ).click(function() {
+/*$( ".nav li" ).click(function() {
     var show = $( this ).attr( "id" ) + "-content";
     var hide = $( ".nav li.active" ).attr( "id" ) + "-content";
 
@@ -32,4 +32,47 @@ $( "#back-icon" ).click(function() {
     $( this ).parent().toggleClass( "show hide" );
     $( "#proj-desc" ).toggleClass( "hide show" );
     $( "#project-list" ).toggleClass( "hide show" );
+});*/
+
+// $('.nav li').on('click', function() {
+//     var show = $( this ).attr( "id" ) + "-content";
+//     var hide = $( ".nav li.active" ).attr( "id" ) + "-content";
+
+//     $( ".nav li.active" ).toggleClass( "active inactive" );
+//     $( "#"+hide ).toggleClass( "hidden" );
+//     $( this ).toggleClass( "inactive active" );
+//     $( "#"+show ).toggleClass( "hidden" );
+// });
+
+$('.nav a').on('click', function(e) {
+    e.preventDefault();
+    var url = this.href;
+
+    $('.nav a li.active').removeClass('active');
+    $(this).children().addClass('active');
+
+    $('#container').remove();
+    $('.content').load(url + ' #container').hide().fadeIn('slow');
 });
+
+$('.content').on('click', '#resources-nav a', function(e) {
+    e.preventDefault();
+    var url = this.href;
+    // var id = '#' + this.parent().parent().attr('id') + '-content';
+
+    $('#resources-nav li.active').removeClass('active');
+    $(this).parent().parent().addClass('active');
+
+    $('#resources-container').remove();
+    $('.resources-content').load(url + ' #resources-container').hide().fadeIn('slow');
+});
+
+// $('.content').on('click', '#resources-nav a', function() {
+//     var show = $(this).attr('id') + '-content';
+//     var hide = $('#resources-nav li.active').attr('id') + '-content';
+
+//     $('#resources-nav li.active').toggleClass('active inactive');
+//     $('#' + hide).toggleClass('hidden');
+//     $(this).toggleClass('inactive active');
+//     $('#' + show).toggleClass('hidden');
+// });
