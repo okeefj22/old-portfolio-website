ea9652341f608553acec19f9ef1f69d9c42e867e
Jacob O'Keeffe <jacob.okeeffe22@mail.dcu.ie>
2015-08-19 09:32:47 +0100

    added content

--- js/main.js
+++ js/main.js
@@ -1,49 +1,3 @@
-/*$( ".nav li" ).click(function() {
-    var show = $( this ).attr( "id" ) + "-content";
-    var hide = $( ".nav li.active" ).attr( "id" ) + "-content";
-
-    $( ".nav li.active" ).toggleClass( "active inactive" );
-    $( "#"+hide ).toggleClass( "hidden" );
-    $( this ).toggleClass( "inactive active" );
-    $( "#"+show ).toggleClass( "hidden" );
-
-});
-
-$( "#resources-nav li" ).click(function() {
-    var show = $( this ).attr( "id" ) + "-content";
-    var hide = $( "#resources-nav li.active" ).attr( "id" ) + "-content";
-
-    $( "#resources-nav li.active" ).toggleClass( "active inactive" );
-    $( "#"+hide ).toggleClass( "hidden" );
-    $( this ).toggleClass( "inactive active" );
-    $( "#"+show ).toggleClass( "hidden" );
-
-});
-
-$( ".proj" ).click(function() {
-    var id = $( this ).attr( "id" ) + "-content";
-
-    $( "#proj-desc" ).toggleClass( "show hide" );
-    $( "#project-list" ).toggleClass( "show hide" );
-    $( "#"+id ).toggleClass( "hide show" );
-});
-
-$( "#back-icon" ).click(function() {
-    $( this ).parent().toggleClass( "show hide" );
-    $( "#proj-desc" ).toggleClass( "hide show" );
-    $( "#project-list" ).toggleClass( "hide show" );
-});*/
-
-// $('.nav li').on('click', function() {
-//     var show = $( this ).attr( "id" ) + "-content";
-//     var hide = $( ".nav li.active" ).attr( "id" ) + "-content";
-
-//     $( ".nav li.active" ).toggleClass( "active inactive" );
-//     $( "#"+hide ).toggleClass( "hidden" );
-//     $( this ).toggleClass( "inactive active" );
-//     $( "#"+show ).toggleClass( "hidden" );
-// });
-
 $('.nav a').on('click', function(e) {
     e.preventDefault();
     var url = this.href;
@@ -73,16 +27,7 @@ $('.content').on('click', '.proj a', function(e) {
     $('#container').remove();
     $('.content').load(url + ' #container').hide().fadeIn('slow');
 
-    // $('#resources-nav li.active').removeClass('active');
-    // $(this).parent().parent().addClass('active');
+    // var c = document.getElementByID('container');
+    // var script = document.getElementByID('syntax');
+    // eval(script.innerHTML);
 });
-
-// $('.content').on('click', '#resources-nav a', function() {
-//     var show = $(this).attr('id') + '-content';
-//     var hide = $('#resources-nav li.active').attr('id') + '-content';
-
-//     $('#resources-nav li.active').toggleClass('active inactive');
-//     $('#' + hide).toggleClass('hidden');
-//     $(this).toggleClass('inactive active');
-//     $('#' + show).toggleClass('hidden');
-// });
